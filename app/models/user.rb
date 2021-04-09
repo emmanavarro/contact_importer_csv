985eb347a555964a36e433d7a8b2fcf148110ea5
Emma Navarro <elnavarro55@gmail.com>
2021-04-09 13:18:45 -0500

    Add association to contacts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :contacts, dependent: :destroy
+
   validates :email, email: true, uniqueness: true, length: { maximum: 100 }
 end
