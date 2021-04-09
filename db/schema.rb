cc73eff14e53b28b8188b7b35431d61eb4d389a6
Emma Navarro <elnavarro55@gmail.com>
2021-04-09 13:18:04 -0500

    Create contact model with association to user model

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,30 @@
-ActiveRecord::Schema.define(version: 20_210_408_151_149) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20_210_409_175_945) do
+  create_table 'contacts', force: :cascade do |t|
+    t.string 'name'
+    t.string 'birth_date'
+    t.string 'phone'
+    t.string 'address'
+    t.string 'credit_card'
+    t.string 'franchise'
+    t.string 'email'
+    t.integer 'user_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['user_id'], name: 'index_contacts_on_user_id'
+  end
+
   create_table 'users', force: :cascade do |t|
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
@@ -10,4 +36,6 @@ ActiveRecord::Schema.define(version: 20_210_408_151_149) do
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
+
+  add_foreign_key 'contacts', 'users'
 end
