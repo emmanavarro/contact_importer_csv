e4403b930102f60d63dba6824cec19fa62b6968e
Emma Navarro <elnavarro55@gmail.com>
2021-04-09 22:10:23 -0500

    Add import method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,16 @@
 class Contact < ApplicationRecord
+
   belongs_to :user
+
+  def self.import(file, user)
+    CSV.foreach(file.path, headers: true) do |row|
+      contact_elements = row.to_h
+      contact = find_or_create_by!(name: contact_elements['name'], birthday: contact_elements['birthday'],
+                                   phone: contact_elements['phone'], address: contact_elements['address'],
+                                   credit_card: contact_elements['credit_card'],
+                                   franchise: contact_elements['franchise'], email: contact_elements['email'],
+                                   user_id: user.id)
+      contact.update(contact_elements)
+    end
+  end
 end
