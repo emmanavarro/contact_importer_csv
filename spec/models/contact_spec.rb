e4f644ed6c9e1f5fdc050850e7fe580e3ec4f30e
Emma Navarro <elnavarro55@gmail.com>
2021-04-12 14:49:55 -0500

    Tests for validations

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    let(:contact) { Contact.new }
+
+    it 'is valid with parameters' do
+      User.create(email: 'emma.navarro@koombea.com', password: '123456')
+      contact = Contact.new(
+        name: 'Yesid Lopez',
+        phone: '(+57) 320-482-73-27',
+        email: 'yesid@example.com',
+        address: 'Cra 47 # 57 -90',
+        birthday: '19911204',
+        credit_card: '15628778935627434',
+        user_id: 1
+      )
+      expect(contact).to be_valid
+    end
+
+    it 'is missing name' do
+      contact.name = nil
+      expect(contact).not_to be_valid
+      expect(contact.errors[:name]).to include("can't be blank")
+    end
+
+    it 'is missing birthday' do
+      contact.birthday = nil
+      expect(contact).not_to be_valid
+      expect(contact.errors[:birthday]).to include("can't be blank")
+    end
+
+    it 'is missing phone' do
+      contact.phone = nil
+      expect(contact).not_to be_valid
+      expect(contact.errors[:phone]).to include("can't be blank")
+    end
+
+    it 'is missing address' do
+      contact.address = nil
+      expect(contact).not_to be_valid
+      expect(contact.errors[:address]).to include("can't be blank")
+    end
+
+    it 'is missing credit card number' do
+      contact.credit_card = nil
+      expect(contact).not_to be_valid
+      expect(contact.errors[:credit_card]).to include("can't be blank")
+    end
+
+    it 'is missing email' do
+      contact.email = nil
+      expect(contact).not_to be_valid
+      expect(contact.errors[:email]).to include("can't be blank")
+    end
+  end
 end
