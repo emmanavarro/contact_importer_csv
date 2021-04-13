76d4d36d6e9b1ae821888d997f859bd7ae241740
Emma Navarro <elnavarro55@gmail.com>
2021-04-12 20:35:41 -0500

    Use shoulda for all validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,25 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.new(email: 'user@example.com', password: '1234567') }
-
   describe 'association' do
     it { should have_many(:contacts).dependent(:destroy) }
   end
 
   describe 'validations' do
-    it 'is valid' do
-      expect(user).to be_valid
-    end
-
-    it 'is not valid without an email' do
-      user.email = nil
-      expect(user).to_not be_valid
-    end
-
-    it 'is not valid without a password' do
-      user.password = nil
-      expect(user).to_not be_valid
-    end
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
   end
 end
