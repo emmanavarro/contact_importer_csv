73b2b7ebee175346006e47327d26b6b3f30a9fc7
Emma Navarro <elnavarro55@gmail.com>
2021-04-13 21:57:08 -0500

    Add encryptation for credit card, failing in save last digits

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -10,6 +10,8 @@ class Contact < ApplicationRecord
   validates :phone, format: { with: VALID_PHONE_REGEX,
                               message: '(+00) 000 000 00 00 or (+00) 000-000-00-00 are the only allowed formats' }
   validates :email, email: true, uniqueness: true, length: { maximum: 100 }
+  # validate :credit_card_info
+  after_validation :credit_card_encrypt
 
   def valid_birthday
     Date.iso8601(birthday)
@@ -22,15 +24,24 @@ class Contact < ApplicationRecord
     errors.add(:file, 'file must be of CSV type.') if file.attached? && !file.content_type.in?(valid_type)
   end
 
+  # def credit_card_info
+  #   self.last_digits = CreditCard.new(last_digits.to_h).card_last_digits
+  # end
+
+  def credit_card_encrypt
+    self.credit_card = CreditCard.new(@credit_card).encrypt
+  end
+
   def self.import(file, user)
     CSV.foreach(file.path, headers: true) do |row|
       contact_elements = row.to_h
       contact = find_or_create_by!(name: contact_elements['name'], birthday: contact_elements['birthday'],
                                    phone: contact_elements['phone'], address: contact_elements['address'],
                                    credit_card: contact_elements['credit_card'],
-                                   franchise: CreditCardValidations::Detector.new(contact_elements['credit_card']).brand_name,
-                                   email: contact_elements['email'],
-                                   user_id: user.id)
+                                   franchise: CreditCardValidations::Detector.new(
+                                     contact_elements['credit_card']
+                                   ).brand_name, last_digits: contact_elements['credit_card'],
+                                   email: contact_elements['email'], user_id: user.id)
       contact.update(contact_elements)
     end
   end
