a49992f7311592c5231cb76e58486acd30644ddf
Emma Navarro <elnavarro55@gmail.com>
2021-04-15 03:54:20 -0500

    Set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
-  get 'contacts/index'
-  resources :contacts do
+  resources :contacts
+  resources :import_files do
     collection { post :import }
   end
+  resources :failed_contacts, only: [:index]
 end
