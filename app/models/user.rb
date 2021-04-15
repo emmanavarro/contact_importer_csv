6936160b25dae64fc47dc1f106d1add85fe37352
Emma Navarro <elnavarro55@gmail.com>
2021-04-15 03:55:41 -0500

    Set associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :contacts, dependent: :destroy
+  has_many :import_files, dependent: :destroy
+  has_many :failed_contacts, dependent: :destroy
 
   validates :email, email: true, uniqueness: true, length: { maximum: 100 }
 end
